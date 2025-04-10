ff72554ddf2456a21ccee8b68595f8ad281f86dd
Everton <username@users.noreply.github.com>
2025-04-09 21:36:20 -0300

    correção home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './pages/Home.js';
+import Home from './pages/home.js';
 import Projetos from './pages/Projetos.js';
 import Personagem from './pages/Personagem.js';
 import Footer from './pages/Footer.js'
